8ffb2c5a2032083819376b6be27139e2e1541987
Lucas <lucas_dd@live.com>
2024-01-17 21:15:59 -0300

    Tipos de dados
    Acrescentando informações sobre tipos de dados em Java

---
+++ src/classes/TiposDeDados.java
@@ -0,0 +1,21 @@
+package classes;
+
+public class TiposDeDados {
+    public static void main(String[] args) {
+
+        System.out.println("Aprendendo POO com Java - Tipos de dados");
+
+        byte tipoByte = 127; // maior número do tipo byte é 127
+        short tipoShort = 32767; // maior número do tipo short é 32767
+        Integer tipoInteger = 2147483647; // maior número do tipo Integer é 2147483647
+        Long tipoLong = 9223372036854775807L ; // tipo utilizado para número gigantescos
+        Float tipoFloat = 2.8F; // tipo utilizado para números com decimais até 7 casas
+        double tipoDouble = 4.89; // tipo utilizado para número com decimais muito grande
+
+        String tipoString = "Texto"; // tipo utilizado para armazenar textos
+        char tipoChar = 'S'; // tipo utilizado para armazenar caracteres
+        boolean tipoBoolean = false; // tipo utilizado para armazenar true ou false
+
+    }
+
+}
